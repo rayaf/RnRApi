5eeb188c81318795f2dbbab51253817ac3390656
Ray Everton <ray.evaf@gmail.com>
2018-08-25 23:05:21 -0300

    add devise and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_version_constraint'
 
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, default: { format: :jason }, constraints: { subdomain: 'api' }, path: '/' do
