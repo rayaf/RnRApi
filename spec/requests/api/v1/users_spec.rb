bebdadcd69d8eceeab1428e578d22d30cb8b0b87
Ray Everton <ray.evaf@gmail.com>
2019-03-30 17:35:45 -0300

    adding create action to the users controllers with tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -32,4 +32,38 @@ RSpec.describe 'Users API', type: :request do
 
   end
 
+  describe 'POST /users' do
+    before do
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+      post '/users', params: { user: user_params }, headers: headers
+    end
+
+    context 'where the request params are valid' do
+      let(:user_params) { attributes_for(:user) }
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'return jason data for the created user' do
+        user_response = JSON.parse(response.body)
+        expect(user_response['email']).to eq(user_params[:email])
+      end
+    end
+
+    context 'where the request params aren`t valid' do
+      let(:user_params) { attributes_for(:user, email: 'invalid') }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'return jason data for the created user' do
+        user_response = JSON.parse(response.body)
+        expect(user_response).to have_key('errors')
+      end
+    end
+  end
+  
+
 end
