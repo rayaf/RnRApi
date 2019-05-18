4e11b61f55af553eb654467a7ffd3a7b0092640e
Ray Everton <ray.evaf@gmail.com>
2019-05-18 14:41:21 -0300

    correcting require location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::API
-    require 'attr_default'
     include Authenticable
 end
--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
+require 'attr_default'
+
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
