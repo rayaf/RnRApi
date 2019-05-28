620ddb5be42745911969a09f32b9335b1b62472b
Ray Everton <ray.evaf@gmail.com>
2019-05-28 20:23:03 -0300

    installing devise_token_auth

--- Gemfile
+++ Gemfile
@@ -57,3 +57,7 @@ gem 'devise'
 gem 'active_model_serializers', '~> 0.10.0'
 
 gem 'ransack'
+
+gem 'devise_token_auth'
+
+gem 'omniauth'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::API
-    include Authenticable
+	include DeviseTokenAuth::Concerns::SetUserByToken
+	include Authenticable
 end
